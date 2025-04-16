717462af5d805b03e11aad4d750536d373e11a75
Brendan McKinley <brendan_mckinley@mines.edu>
2025-04-16 14:26:21 -0600

    use wordnet

--- test.py
+++ test.py
@@ -1,21 +1,19 @@
 from utils import getStrongestFeatureForWord, getWeakestWordForFeature
+from nltk.corpus import wordnet
 
 model_id = "gpt2-small"
-target_word = "upset"
-syonyms = ["trouble",
-"perturb",
-"discompose",
-"unsettle",
-"disconcert",
-"distress",
-"discountenance",
-"dismay",
-"disquiet",
-"worry",
-"bother",
-"plague"]
+prompt = "The quick brown fox jumps over the lazy dog"
+
+target_word = "quick" # TO DO: compute word saliency scores for each word in prompt and choose most important word
+
+synonyms = [ target_word ]
+for synonym in wordnet.synsets(target_word):
+    for lemma in synonym.lemmas():
+        word = lemma.name()
+        if word not in synonyms:
+            synonyms.append(word)
 
 layer, index = getStrongestFeatureForWord(model_id, target_word)
-weakest_synonym = getWeakestWordForFeature(model_id, layer, index, syonyms)
+weakest_synonym = getWeakestWordForFeature(model_id, layer, index, synonyms)
 
 print("Weakest synonym: ", weakest_synonym)
